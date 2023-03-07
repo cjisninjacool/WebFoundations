0fc00cdf4a7a89de7b0d050f8837f93d4d7f3a54
Christian Hart <60236591+cjisninjacool@users.noreply.github.com>
2023-03-07 08:43:18 -0800

    101 assignment 102-2 assignment

---
+++ 101-2-assignment/index.ts
@@ -0,0 +1,7 @@
+let contact = {
+    contactName: "Bob",
+    dob: "02/02/94",
+    role: "student"
+}
+let { contactName , dob , role } = contact
+console.log(contactName+" is a "+ role +" learning to code and was born on "+dob);
---
+++ 102-2-assignment/index.ts
@@ -0,0 +1,12 @@
+class Employee {
+    name: string;
+    title: string;
+    department: string;
+    salary: number;
+    constructor(name: string, title: string, department: string, salary: number){
+        this.name = name;
+        this.title = title;
+        this.department = department;
+        this.salary = salary;
+    }
+}
